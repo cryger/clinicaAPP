5409c5583b28122e2e16d55f596d5aa39c961062
Henry Alfredo Garcia Palacio <34179646+cryger@users.noreply.github.com>
2024-09-19 04:29:16 -0500

    actualizacion de repositorio

---
+++ Database.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ClinicaAPP
+{
+    public class Database
+    {
+        private MySqlConnection connection;
+
+        // Constructor: Inicia la conexión con la base de datos
+        public Database()
+        {
+            string connectionString = "Server=localhost;Database=clinica;User ID=root;Password='';";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        // Método para abrir la conexión
+        public void OpenConnection()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir la conexión: " + ex.Message);
+            }
+        }
+
+        // Método para cerrar la conexión
+        public void CloseConnection()
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cerrar la conexión: " + ex.Message);
+            }
+        }
+
+        // Obtener la conexión
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+
+
+    }
+}
